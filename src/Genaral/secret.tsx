6e6e7366e4905ddf5eaab01b827880321f64adfc
eswaraprasad <esweraprasad@gmail.com>
2026-02-06 23:02:23 +0530

    business name change

--- src/Genaral/secret.tsx
+++ src/Genaral/secret.tsx
@@ -3,7 +3,7 @@ export const whatsapp_number = "+916363600791";
 export const phone_number2 = "+917676384744";
 export const whatsapp_number2 = "+917676384744";
 
-export const business_name = "Kerala Astro Center";
+export const business_name = "Kerala Bhagavathi Astro";
 export const person_name = "Pandith Raghunandan Poduval";
 
 export const email_address = "";
